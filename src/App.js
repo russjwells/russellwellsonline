e028f0990895f490972a0fcb619f175a2f8321a2
Russ Wells <russ@russdigital.com>
2017-01-23 08:32:18 -0800

    switched hashHistory to browserHistory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import Site from './Site.js';
 import Resume from './Resume.js';
 import Showcase from './Showcase.js'
@@ -10,7 +10,7 @@ import About from './About.js'
 class App extends Component {
   render() {
     return (
-      <Router history={ hashHistory }>
+      <Router history={ browserHistory }>
         <Route path="/" component={Site}>
           <IndexRoute component={Resume}></IndexRoute>
           <Route path="showcase" component={Showcase}></Route>
