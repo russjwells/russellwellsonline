87e739c081d3fc71d1a5d6ef5ce2e1f50f41cdfb
Russ Wells <russ@russdigital.com>
2017-01-23 03:36:22 -0800

    updated project list and about

--- src/About.js
+++ src/About.js
@@ -6,15 +6,22 @@ class About extends Component {
         <div className="About">
           <div className="AboutSpacer">
             <h2>Making an Engineer</h2>
-            <p>
+
+            {/*<p>
               I began programming when I was young. In middle school I learned Basic on the TI-83, then HTML from Neopets and began to create my own geocities sites. In high school I took all the programming courses available and created an independent study. Before graduation I had AP computer science credit and a working knowledge of Visual Basic, C, C#, and Java, as well as HTML, CSS, PHP and mySQL.
              </p>
              <p>
               I was accepted into and enrolled in the Digtal Design program in DAAP at the University of Cincinnati. I spent the first year in design fundementals learning the theories of color form and space, the principals of organization, and the elements of design. In my second year I began working extensively in Flash and Actionscript 3, started picking up Javascript, learned jQuery, and some animation libaries. I worked in Venice, CA at a Motion Graphics studio over the Summer. In my third year I lead development on several Flash based apps for smartphones, and founded my first startup.
+             </p>*/}
+             <p>
+              I began creating my first startup while studying digital design at the University of Cincinnati. I was inspired by public transit debate in Cincinnati and the different art scenes in Ohio's biggest cities to create Rootless, a peer to peer ride sharing network. Rootless went on to attend the Betaspring startup accelerator in Fall of 2012. It grew into its place in the startup scene competing with Uber and Lyft (then Zimride), and created three jobs in Rhode Island. Rootless served patrons of music and art festivals across America over the Summer of 2013.
              </p>
              <p>
-
+              After my first startup I left Rhode Island, went back to Cincinnati to finish my Bachelor's of Science in Design, and moved to San Diego to program Javascript for the web.
              </p>
+             {/*<p>
+               In addition to websmithing, I like to program C# in Unity, make beats, and enjoy nature.
+             </p>*/}
           </div>
         </div>
     );
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -6,9 +6,6 @@ class ProjectList extends Component{
     return(
       <div className="ProjectList">
         <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={sittingpic} alt="img" width="400" height="400" />
-          </div>
           <div className="ProjectInfo">
             <h2>Sitting</h2>
             <p>A multi-platform meditation timer and tracker.</p>
@@ -19,21 +16,6 @@ class ProjectList extends Component{
         </div>
 
         <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={""} alt="img" width="400" height="400" />
-          </div>
-          <div className="ProjectInfo">
-            <h2>Twitter Clone</h2>
-            <p>I recreated the functionality of Twitter.</p>
-            <p><i>mongo, angular, express, node</i></p>
-            <p><a href="https://twittercl0ne.herokuapp.com/">Web App</a> | <a href="https://github.com/russjwells/sittingweb">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={""} alt="img" width="400" height="400" />
-          </div>
           <div className="ProjectInfo">
             <h2>Reddit Clone</h2>
             <p>Authentication, Upvotes, Comments, Comment Upvotes</p>
@@ -43,42 +25,40 @@ class ProjectList extends Component{
         </div>
 
         <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={""} alt="img" width="400" height="400" />
-          </div>
           <div className="ProjectInfo">
             <h2>Many Timers</h2>
             <p>You can time everything at once right here.</p>
             <p><i>react</i></p>
-            <p><a href="">Web App</a> | <a href="https://github.com/russjwells/time_tracking_app">Repository</a></p>
+            <p><a href="https://github.com/russjwells/time_tracking_app">Repository</a></p>
           </div>
         </div>
 
         <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={""} alt="img" width="400" height="400" />
-          </div>
-          <div className="ProjectInfo">
-            <h2>Github Notetaker</h2>
-            <p>Search github by username. Take notes on users locally.</p>
-            <p><i>react</i></p>
-            <p><a href="">Web App</a> | <a href="https://github.com/russjwells/notetaker">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectImage">
-            <img src={""} alt="img" width="400" height="400" />
-          </div>
           <div className="ProjectInfo">
             <h2>Voting App</h2>
             <p>upvotes and downvotes</p>
             <p><i>react</i></p>
-            <p><a href="">Web App</a> | <a href="https://github.com/russjwells/voting_app">Repository</a></p>
+            <p><a href="https://github.com/russjwells/voting_app">Repository</a></p>
           </div>
         </div>
 
+        <div className="ProjectListItem">
+          <div className="ProjectInfo">
+            <h2>Github Notetaker</h2>
+            <p>Search github by username. Take notes on users locally.</p>
+            <p><i>react</i></p>
+            <p><a href="https://github.com/russjwells/notetaker">Repository</a></p>
+          </div>
+        </div>
 
+        <div className="ProjectListItem">
+          <div className="ProjectInfo">
+            <h2>Twitter Clone</h2>
+            <p>I recreated the functionality of Twitter.</p>
+            <p><i>mongo, angular, express, node</i></p>
+            <p><a href="https://twittercl0ne.herokuapp.com/">Web App</a> | <a href="https://github.com/russjwells/sittingweb">Repository</a></p>
+          </div>
+        </div>
       </div>
     )
   }
