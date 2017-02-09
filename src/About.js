fadda1756c4ce0504c3e30db3eea4e192ab0f6b2
Russ Wells <russ@russdigital.com>
2017-02-09 14:29:58 -0800

    more copy changes

--- src/About.js
+++ src/About.js
@@ -15,15 +15,21 @@ class About extends Component {
               I was accepted into and enrolled in the Digtal Design program in DAAP at the University of Cincinnati. I spent the first year in design fundementals learning the theories of color form and space, the principals of organization, and the elements of design. In my second year I began working extensively in Flash and Actionscript 3, started picking up Javascript, learned jQuery, and some animation libaries. I worked in Venice, CA at a Motion Graphics studio over the Summer. In my third year I lead development on several Flash based apps for smartphones, and founded my first startup.
              </p>*/}
              <p>
-              I began creating my first startup while studying digital design at the University of Cincinnati. I was inspired by public transit debate in Cincinnati and the different art scenes in Ohio's biggest cities to create Rootless, a peer to peer ride sharing network. Rootless went on to attend the Betaspring startup accelerator in Fall of 2012. It grew into its place in the startup scene competing with Uber and Lyft (then Zimride), and created three jobs in Rhode Island. Funded, Rootless served patrons of music and art festivals across America over the Summer of 2013.
+              I created my first startup while studying digital design at the University of Cincinnati. I was inspired by public transit debate in Cincinnati and the different art scenes in Ohio's biggest cities to create Rootless, a peer to peer ride sharing network. Rootless went on to attend the Betaspring startup accelerator in Fall of 2012. It grew into its place in the startup scene competing with Uber and Lyft (then Zimride). Rootless created three jobs in Rhode Island, and served patrons of music and art festivals across America over the Summer of 2013.
              </p>
              <p>
               After my first startup I left Rhode Island, went back to Cincinnati to finish my Bachelor's of Science in Design, and moved to San Diego to program Javascript for the web.
              </p>
+             <p>
+              I take digital products from inception to completion, and bring your dreams and ideas to life.
+             </p>
+             <p>I have five years of professional design and engineering experience. I have worked on everything from motion graphics for major television networks to product websites for startups, digital storefronts for local businesses, native mobile applications, and 3D games. I'd love to work with you! Email me for my current rate.</p>
+             <p>&nbsp;</p>
+             <p>Russell</p>
              {/*<p>
                In addition to websmithing, I like to program C# in Unity, make beats, and enjoy nature.
              </p>*/}
-             <p>&nbsp;</p>
+
              <p>
              <Follow
                username={"russellwells"}
