ce370d5d95f5adad0c1ed13d319b22a599f02264
Russ Wells <russ@russdigital.com>
2017-12-08 11:31:58 -0800

    updated contact info

--- src/Leftside.js
+++ src/Leftside.js
@@ -7,9 +7,9 @@ class Leftside extends Component {
       <div className="Leftside">
         <img src={headshot} alt="headshot" width="394" height="526"/>
         <div className="QuickContacts">
-          <p>russ@russdigital.com</p>
+          <p>russjwells@gmail.com</p>
           <p>github.com/russjwells</p>
-          <p>(513) 332 5252</p>
+          <p>(760) 936 1704</p>
         </div>
         <div className="Message">
           message
