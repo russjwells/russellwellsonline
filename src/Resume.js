bf42e7a145155158913638c8fa572134482ed2da
Russ Wells <russ@russdigital.com>
2019-10-23 15:46:16 -0700

    resume link

--- src/Resume.js
+++ src/Resume.js
@@ -6,7 +6,7 @@ class Resume extends Component {
       <div className="Resume">
         <div className="resumeSpacer">
           <h2>Fall 2019</h2>
-          <p><a href="https://www.dropbox.com/s/wcghvbb64c1mazo/rw_cv_2019f.pdf?dl=1">download the .pdf</a></p>
+          <p><a href="https://www.dropbox.com/s/seq6fgi7efqub63/russellwells_cv_2019f.pdf?dl=1">download the .pdf</a></p>
           {/*<h2>Product Design & UX</h2>
           <p><a href="https://www.dropbox.com/s/lq0f5tsrdrfsn8i/rw_cv_2019s_ux.pdf?dl=1">download the .pdf</a></p>
           <h2>Hybrid Positions</h2>
