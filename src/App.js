74d6c4ea0791321983d490ccb301d5e9ff70a1ac
Russ Wells <russ@russdigital.com>
2019-10-29 15:05:36 -0700

    building out working analytics. also updated the favicon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { createBrowserHistory } from 'history';
 import Site from './Site.js';
 import Resume from './Resume.js';
 import Software from './Software.js'
 import Design from './Design.js'
 import About from './About.js'
 
-import ReactGA from 'react-ga'
 
-ReactGA.pageview(window.location.pathname + window.location.search)
+
+import ReactGA from 'react-ga'
+//const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+browserHistory.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import ReactGA from 'react-ga'
-
+console.log('first')
 ReactGA.initialize('UA-131525485-5', { standardImplementation: true, siteSpeedSampleRate: 100 })
 
 ReactDOM.render(
