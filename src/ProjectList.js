f094ea22a4fa3336b3e3a81e56ce680d536afc47
Russ Wells <russ@russdigital.com>
2020-10-16 22:26:01 -0700

    arranged projects

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -17,7 +17,9 @@ class ProjectList extends Component{
       <div className="ProjectList">
         <div className="ProjectListItem">
             <div className="ProjectPhoto">
+              <a target="_blank" href="https://www.sexyawakening.com">
               <img src={sexyawakening} alt="missing image" width="300px" height="300px"/>
+              </a>
             </div>
             <div className="ProjectInfo">
               <h2>Sexy Awakening</h2>
@@ -29,7 +31,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
+            <a target="_blank" href="https://www.damnjoan.com">
               <img src={damnjoan} alt="missing image" width="300px" height="300px"/>
+            </a>
             </div>
           <div className="ProjectInfo">
             <h2>Damn Joan</h2>
@@ -41,7 +45,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
+              <a target="_blank" href="https://www.sitting.app">
               <img src={sitting} alt="missing image" width="300px" height="300px"/>
+              </a>
             </div>
           <div className="ProjectInfo">
             <h2>Sitting</h2>
@@ -54,8 +60,23 @@ class ProjectList extends Component{
           </div>
         </div>
         <div className="ProjectListItem">
+          <div className="ProjectPhoto">
+            <a target="_blank" href="https://www.behance.net/gallery/78154521/The-VALIS-Mandala">
+              <img src={valis} alt="missing image" width="300px" height="300px"/>
+            </a>
+            </div>
+          <div className="ProjectInfo">
+            <h2>VALIS Flyer</h2>
+            <p><a target="_blank" href="https://www.behance.net/gallery/78154521/The-VALIS-Mandala">The VALIS Mandala</a></p>
+            <p>I created this printed digital art piece as the invitation to a metal show with a VR installation in the lobby.</p>
+            <p><i>Nikon DSLR, Cinema 4D, Photoshop, Illustrator</i></p>
+          </div>
+        </div>
+        <div className="ProjectListItem">
             <div className="ProjectPhoto">
-              <img src={chromadrum} alt="missing image" width="300px" height="300px"/>
+              <a target="_blank" href="https://www.chromadrum.com">
+                <img src={chromadrum} alt="missing image" width="300px" height="300px"/>
+              </a>
             </div>
             <div className="ProjectInfo">
               <h2>Chroma Drum</h2>
@@ -67,7 +88,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
-              <img src={galacticzoo} alt="missing image" width="300px" height="300px"/>
+              <a target="_blank" href="https://galacticzoo.herokuapp.com">
+                <img src={galacticzoo} alt="missing image" width="300px" height="300px"/>
+              </a>
             </div>
           <div className="ProjectInfo">
             <h2>The Galactic Zoo</h2>
@@ -78,7 +101,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
+          <a target="_blank" href="http://www.battlehigh2.com">
               <img src={battlehigh2} alt="missing image" width="300px" height="300px"/>
+          </a>
             </div>
           <div className="ProjectInfo">
             <h2>Battle High 2 Website</h2>
@@ -89,7 +114,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
+              <a target="_blank" href="#">
               <img src={prometheantemples} alt="missing image" width="300px" height="300px"/>
+              </a>
             </div>
           <div className="ProjectInfo">
             <h2>Promethean Temples</h2>
@@ -100,7 +127,9 @@ class ProjectList extends Component{
         </div>
         <div className="ProjectListItem">
           <div className="ProjectPhoto">
+            <a target="_blank" href="https://www.youtube.com/watch?v=-8szQw0YyBA&t=5s">
               <img src={harmonize} alt="missing image" width="300px" height="300px"/>
+            </a>
             </div>
           <div className="ProjectInfo">
             <h2>Harmonize</h2>
@@ -109,124 +138,6 @@ class ProjectList extends Component{
             <p><i>Unity3D, Cinema 4D, JavaScript, Photoshop, Illustrator</i></p>
           </div>
         </div>
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={valis} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>VALIS Flyer</h2>
-            <p><a target="_blank" href="https://www.behance.net/gallery/78154521/The-VALIS-Mandala">The VALIS Mandala</a></p>
-            <p>I created this printed digital art piece as the invitation to a metal show with a VR installation in the lobby.</p>
-            <p><i>Nikon DSLR, Cinema 4D, Photoshop, Illustrator</i></p>
-          </div>
-        </div>
-        {/*
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Galactic Zoo</h2>
-            <p><a target="_blank" href="https://www.sitting.app">www.sitting.app</a></p>
-            <p>Meditation for the Masses.</p>
-            <p><i>react, react native, node, express</i></p>
-            <p><a target="_blank" href="https://sitting.app">Web App</a> | <a target="_blank" href="https://github.com/russjwells/sittingweb">Repository</a></p>
-            <p><a target="_blank" href="https://itunes.apple.com/us/app/sitting-timer/id1139439714">iOS App</a> | <a target="_blank" href="https://github.com/russjwells/SittingApp">Repository</a></p>
-            <p>Android app coming!</p>
-          </div>
-        </div>
-        
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Promethean Temples</h2>
-            <p><a target="_blank" href="https://www.sitting.app">www.sitting.app</a></p>
-            <p>Meditation for the Masses.</p>
-            <p><i>react, react native, node, express</i></p>
-            <p><a target="_blank" href="https://sitting.app">Web App</a> | <a target="_blank" href="https://github.com/russjwells/sittingweb">Repository</a></p>
-            <p><a target="_blank" href="https://itunes.apple.com/us/app/sitting-timer/id1139439714">iOS App</a> | <a target="_blank" href="https://github.com/russjwells/SittingApp">Repository</a></p>
-            <p>Android app coming!</p>
-          </div>
-        </div>
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Harmonize</h2>
-            <p><a target="_blank" href="https://www.sitting.app">www.sitting.app</a></p>
-            <p>Meditation for the Masses.</p>
-            <p><i>react, react native, node, express</i></p>
-            <p><a target="_blank" href="https://sitting.app">Web App</a> | <a target="_blank" href="https://github.com/russjwells/sittingweb">Repository</a></p>
-            <p><a target="_blank" href="https://itunes.apple.com/us/app/sitting-timer/id1139439714">iOS App</a> | <a target="_blank" href="https://github.com/russjwells/SittingApp">Repository</a></p>
-            <p>Android app coming!</p>
-          </div>
-        </div>
-        */}
-        {
-          /*
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Reddit Clone</h2>
-            <p>Authentication, Upvotes, Comments, Comment Upvotes</p>
-            <p><i>mongo, angular, express, node</i></p>
-            <p><a target="_blank" href="https://redditcl0ne.herokuapp.com/">Web App</a> | <a target="_blank" href="https://github.com/russjwells/redditclone">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Twitter Clone</h2>
-            <p>I recreated the functionality of Twitter.</p>
-            <p><i>mongo, angular, express, node</i></p>
-            <p><a target="_blank" href="https://twittercl0ne.herokuapp.com/">Web App</a> | <a htarget="_blank" ref="https://github.com/russjwells/sittingweb">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Many Timers</h2>
-            <p>You can time everything at once with this.</p>
-            <p><i>react</i></p>
-            <p><a target="_blank" href="https://github.com/russjwells/time_tracking_app">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Voting App</h2>
-            <p>upvotes and downvotes</p>
-            <p><i>react</i></p>
-            <p><a target="_blank" href="https://github.com/russjwells/voting_app">Repository</a></p>
-          </div>
-        </div>
-
-        <div className="ProjectListItem">
-          <div className="ProjectPhoto">
-              <img src={missingimg} alt="missing image" width="300px" height="300px"/>
-            </div>
-          <div className="ProjectInfo">
-            <h2>Github Notetaker</h2>
-            <p>Search github by username. Take notes on users locally.</p>
-            <p><i>react</i></p>
-            <p><a target="_blank" href="https://github.com/russjwells/notetaker">Repository</a></p>
-          </div>
-        </div>
-        */}
       </div>
     )
   }
